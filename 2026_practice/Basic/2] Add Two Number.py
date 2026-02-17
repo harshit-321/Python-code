ed2da98831e27f78259f6d52c775c27af30afb41
Harshit Rathi <harshitrathi321@gmail.com>
2026-02-17 22:38:35 +0530

    Add files via upload

---
+++ 2026_practice/Basic/2] Add Two Number.py
@@ -0,0 +1,9 @@
+#write a program print addition of two no.
+a= int(input("enter the value : "))
+b=int(input("enter the value : "))
+print('total sum is: ',a+b)
+
+
+
+
+
---
+++ 2026_practice/Basic/3] Area & Parameter.py
@@ -0,0 +1,12 @@
+#write of programe area and parameter
+a=int(input("enter the length : "))
+b=int(input("enter the breath : "))
+
+#rectangle 
+print("Area of rectangle : ",a*b)
+print("Parameter of rectangle : ",2*(a+b))
+
+#square
+print("Area of Square : ",a**a)
+print("Parameter Square : ",4*a)
+
